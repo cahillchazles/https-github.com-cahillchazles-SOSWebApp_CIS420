af303f1368bf6f41ab125ac6f3245ea251157958
Adam R Clary <adamclary59@gmail.com>
2018-01-30 16:12:50 -0500

    GetPhysician Added

---
+++ SOSWebApp/Controllers/VolunteersFormController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class VolunteersFormController : Controller
+    {
+        // GET: VolunteersForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetPhysicianForm()
+        {
+            return View();
+        }
+    }
+}
