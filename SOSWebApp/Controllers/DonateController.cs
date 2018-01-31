8b1f0306c10dc9e76dabd395563ee3c5da08d463
Spalding,Zachary Thomas <ztspal01@louisville.edu>
2018-01-31 15:43:57 -0500

    Added more to the Partner Clinics page, also put in the donate option

---
+++ SOSWebApp/Controllers/DonateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class DonateController : Controller
+    {
+        // GET: Donate
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetDonate()
+        {
+            return View();
+        }
+    }
+}
