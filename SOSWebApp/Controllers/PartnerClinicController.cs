7709df97e3513bd4f098edaf8ad3c00ab1849831
Spalding,Zachary Thomas <ztspal01@louisville.edu>
2018-01-31 00:08:27 -0500

    Added GetHelp functionality

---
+++ SOSWebApp/Controllers/PartnerClinicController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class PartnerClinicController : Controller
+    {
+        // GET: PartnerClinic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPartnerClinic()
+        {
+            return View();
+        }
+    }
+}
