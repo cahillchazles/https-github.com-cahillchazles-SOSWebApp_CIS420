e33521aa8f2c8ec84aa3e39e1a3a775ed19e58ab
Kevin C Burdette <kcburd01@louisville.edu>
2018-02-01 16:19:18 -0500

    Refer a Patient

---
+++ SOSWebApp/Controllers/ReferAPatientController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class ReferAPatientController : Controller
+    {
+        // GET: ReferAPatient
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ReferAPatient()
+        {
+            return View();
+        }
+    }
+}
