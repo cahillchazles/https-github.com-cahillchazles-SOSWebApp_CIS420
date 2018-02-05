9c1e18754578d2a78f2bfa63569eae4322471e4a
Adam R Clary <adamclary59@gmail.com>
2018-02-04 22:05:35 -0500

    Added staff access controller. Some changes to footer and volunteer forms.

---
+++ SOSWebApp/Controllers/StaffAccessController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class StaffAccessController : Controller
+    {
+        // GET: StaffAccess
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetLoginScreen()
+        {
+            return View();
+        }
+        public ActionResult StaffAccessScreen()
+        {
+            return View();
+        }
+    }
+}
