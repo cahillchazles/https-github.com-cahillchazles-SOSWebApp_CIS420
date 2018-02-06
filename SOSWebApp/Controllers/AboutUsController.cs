adcff40fa327c42ae12826e62e2eea51a7503f60
Clark A Meyer <cameye06@louisville.edu>
2018-02-06 12:55:35 -0800

    added about us stuff

---
+++ SOSWebApp/Controllers/AboutUsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult aboutUs()
+        {
+            return View();
+        }
+        public ActionResult staff()
+        {
+            return View();
+        }
+        public ActionResult stories()
+        {
+            return View();
+        }
+        public ActionResult board()
+        {
+            return View();
+        }
+        public ActionResult testimonials()
+        {
+            return View();
+        }
+    }
+}
