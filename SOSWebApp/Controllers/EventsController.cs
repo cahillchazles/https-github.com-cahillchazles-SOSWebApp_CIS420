652fd1b8a8116852ffa3bce0b8e9ae399c60a3bc
Adam R Clary <adamclary59@gmail.com>
2018-02-06 11:18:42 -0500

    Calendar Added

---
+++ SOSWebApp/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSWebApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCalendar()
+        {
+            return View();
+        }
+    }
+}
