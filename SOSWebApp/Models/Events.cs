560f48bbd115c58b1db02e599304bc6d345823c7
Adam R Clary <adamclary59@gmail.com>
2018-02-15 17:02:45 -0500

    Events Model

---
+++ SOSWebApp/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SOSWebApp.Models
+{
+    public class Events
+    {
+        public int ID { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventLocation { get; set; }
+
+    }
+}
