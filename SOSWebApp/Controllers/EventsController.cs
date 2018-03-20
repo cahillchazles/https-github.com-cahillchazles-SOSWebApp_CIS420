f44abee9a6cb3e0ab8a4b2a1061b2129e9246550
Matthew  Boling <mjboli02@louisville.edu>
2018-03-20 15:13:05 -0400

    Changed Google Calendar to Events List

--- SOSWebApp/Controllers/EventsController.cs
+++ SOSWebApp/Controllers/EventsController.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SOSWebApp.Models;
+
+
 
 namespace SOSWebApp.Controllers
 {
     public class EventsController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult GetCalendar()
+        {
+            return View(db.Events.ToList());
+        }
+
         // GET: Events
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetCalendar()
-        {
-            return View();
-        }
+
     }
 }
+
+
